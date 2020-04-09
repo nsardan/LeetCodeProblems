c9d744dc1e3186ed7cbda52dbec5a29bb74fcebd
nsardana <neha.nsit@gmail.com>
2020-04-08 23:42:34 -0400

    ClimbStairs.java

---
+++ src/main/java/com/learning/dynamicprogramming/ClimbStairs.java
@@ -0,0 +1,26 @@
+package com.learning.dynamicprogramming;
+
+public class ClimbStairs {
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        ClimbStairs main = new ClimbStairs();
+        int a = main.climbStairs(10);
+        System.out.println(a);
+
+    }
+}
