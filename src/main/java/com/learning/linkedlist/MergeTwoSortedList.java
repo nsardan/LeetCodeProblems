79953e47204438cb705ba8e0c2ddc5e567a924f4
nsardana <neha.nsit@gmail.com>
2020-04-14 00:05:44 -0400

    MergeTwoSortedList

---
+++ src/main/java/com/learning/linkedlist/MergeTwoSortedList.java
@@ -0,0 +1,36 @@
+package com.learning.linkedlist;
+
+public class MergeTwoSortedList {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        else if(l2 == null) return l1;
+        else if(l1.val < l2.val)
+        {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else{
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        MergeTwoSortedList main = new MergeTwoSortedList();
+        ListNode finalAns = main.mergeTwoLists(l1, l2);
+
+        while(finalAns != null){
+            System.out.println(finalAns.val);
+            finalAns = finalAns.next;
+        }
+
+    }
+}
