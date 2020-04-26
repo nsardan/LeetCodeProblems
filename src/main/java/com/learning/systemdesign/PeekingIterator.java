87096283000550dc7228bbfb0eec37e933b7b5ce
nsardana <neha.nsit@gmail.com>
2020-04-25 22:28:50 -0400

    PeekingIterator

---
+++ src/main/java/com/learning/systemdesign/PeekingIterator.java
@@ -0,0 +1,42 @@
+package com.learning.systemdesign;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class PeekingIterator implements Iterator<Integer> {
+
+    private Iterator<Integer> iter;
+    private Integer next = null;
+
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
+        if(iterator.hasNext()){
+            this.next = iterator.next();
+        }
+        this.iter = iterator;
+
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return next;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        if(next == null) throw new NoSuchElementException();
+        Integer toReturn = next;
+        next = null;
+        if(iter.hasNext())
+            next = iter.next();
+        return toReturn;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return (next != null);
+    }
+}
